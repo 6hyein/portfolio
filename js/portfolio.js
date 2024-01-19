d19a9428df3c73bbbe48a09acf76ed74e7b0c8ed
6hyein <6hyein2013@gmail.com>
2024-01-19 23:22:53 +0900

    포폴2sec2완

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
         $("#menuBtn").toggleClass("menuClick");
         $(".menuList").slideToggle().css("display","flex");
     });
+    // 버튼 클릭시 링크 이동
+    let btn = document.querySelectorAll(".btn");
+    btn.forEach((element) => {
+        // 버튼인 경우만
+        if(element.tagName === "BUTTON"){
+            element.onclick = () => {
+                // 해당 링크로 페이지 이동
+                let btnHref = element.getAttribute('href');
+                window.location.href = btnHref;
+            }
+        }
+    });
 });
