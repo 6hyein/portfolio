8c85695d44d9b59b6de465e2785dc0f3b1f372e8
6hyein <6hyein2013@gmail.com>
2024-01-28 00:14:07 +0900

    포폴2메인으로

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,63 +15,24 @@ $(document).ready(function(){
             }
         }
     });
-    // 스킬바
-    const myChart2 = document.getElementById("myChart");
-    const chartArea = myChart2.getContext('2d');
-    let labelsList = myChart2.children;
-    let labels = Array();
-    let datas = Array(); 
-    let backgroundColors = Array(); 
-    let colors = Array();
-
-    for (let val in labelsList){
-        if( !isNaN(val) ){
-            labels.push(labelsList[val].getAttribute('data_text'));
-            datas.push(labelsList[val].getAttribute('data-value'));
-            colors.push("rgb("+labelsList[val].getAttribute('data-color')+")");
-            backgroundColors.push("rgba("+labelsList[val].getAttribute('data-color')+",0.2)");
+   
+     // 스킬바
+    const skillsTop = $("#skills").offset().top-300;
+    const projectsTop = $("#projects").offset().top;
+    let scrollEvn = () => {
+        let scrollTop = $(document).scrollTop();
+        if(skillsTop <= scrollTop && projectsTop >= scrollTop){
+            $(".bar").each((idx,item)=>{                
+                let barWidth = $(item).attr("bar-width");
+                $(item).css("width",barWidth);
+            });
+        }else{
+            $(".bar").css("width",0);
         }
     }
-    var myChart = new Chart(chartArea, {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'skill level',
-                data: datas,
-                backgroundColor: backgroundColors,
-                borderColor: colors,
-                borderRadius: 10,
-                barThickness:30,
-                borderWidth: 2
-            }]
-        },
-        options: {
-            responsive: false, // 크기 조정 
-            maintainAspectRatio: false, // 가로 세로 종횡비 유지
-            indexAxis: 'y',
-            plugins:{
-                legend: {
-                    display: false
-                },
-                tooltip: {
-                    enabled: false // <-- this option disables tooltips
-                }
-            },
-            scales: {
-                x: {
-                    ticks:{
-                        color: '#fff'
-                    }
-                },
-                y: {
-                    ticks:{
-                        color: '#fff',
-                        categoryPercentage: 0.7,
-                        maxBarThickness: 20,
-                    }
-                }
-            }
-        }
+    scrollEvn();
+    $(document).scroll(function(){
+        scrollEvn();
     });
+ 
 });
